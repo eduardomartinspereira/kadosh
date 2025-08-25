5edcbb7581526b8b3327f1a983d43a4615e8b5fd
DiogoPGR <diogovitalis2022@gmail.com>
2025-08-25 16:04:32 -0300

    pagamento

--- app/api/paymentstatus/route.ts
+++ app/api/paymentstatus/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { mercadoPagoService } from '../../lib/mercadopago';
+import { mercadoPagoService } from '../../../lib/mercadopago';
 
 export const runtime = 'nodejs';
 
